946c7b1e84f1bc07e600aa2bd7aae64044c01719
msrnayeem <msrnayeem@gmail.com>
2026-02-26 15:56:42 +0600

    clean

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,15 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-Route::get('/verify', function () {
-    return view('index');
-})->name('verify');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
 
 Route::get('/profile', function () {
     return view('profile');
 })->name('profile');
-
-require __DIR__.'/auth.php';
